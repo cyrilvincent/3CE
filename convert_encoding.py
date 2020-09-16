255c97f1199f77cdf4e97c05c1e5e13da58c6e94
cyrilvincent <contact@cyrilvincent.com>
2020-09-16 12:36:35 +0200

    Doc

--- convert_encoding.py
+++ convert_encoding.py
@@ -1,8 +1,9 @@
 #pip install chardet
 #chardetect fichier-cyril.txt
 #fichier-cyril.txt: ISO-8859-1 with confidence 0.73
-with open("data/fichier-cyril.txt", encoding="ISO-8859-1") as f:
+with open("data/fichier-cyril.txt", "r", encoding="ISO-8859-1") as f:
     s = f.read()
-    with open("data/data-utf8.txt", "w", encoding="utf8") as f2:
-        f2.write(s)
+    with open("data/data.txt", "wb") as f2:
+        f2.write(s.encode("utf8"))
+
 
---
+++ nppredict.py
@@ -0,0 +1,19 @@
+import time
+import cyrilload
+from npnearest import NPNearest
+
+print("NPPredict")
+print("=========")
+np = NPNearest("data/data.h.pickle")
+while True:
+    s = input("Enter a sentence: ")
+    best = (None, 0)
+    for k in np.db.keys():
+        p = np.get_by_id(k)
+        score = np.comp.compvl(s, p.l[0].val)
+        if score > best[1]:
+            best = (p, score)
+    val = [c.val for c in best[0].l if c.id == 995]
+    print(f"Predict carac 955 {val} at {best[1]*100:.0f}%")
+
+
