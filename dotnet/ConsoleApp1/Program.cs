bb85f8a858622ed9bf5ab2b7341396c5bba7656d
cyrilvincent <contact@cyrilvincent.com>
2020-09-17 14:51:00 +0200

    .NET

---
+++ dotnet/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Py.GIL())
+            {
+                Environment.SetEnvironmentVariable("PYTHONPATH", @"C:\Users\conta\CVC\3C-Evolution\git3ce");
+                PythonEngine.PythonPath = Environment.GetEnvironmentVariable("PYTHONPATH");
+                dynamic sys = Py.Import("sys");
+                sys.path.append(@"C:\Users\conta\CVC\3C-Evolution\git3ce");
+                dynamic config = Py.Import("config");
+                Console.WriteLine(config.version);
+                dynamic npnearest = Py.Import("npnearest");
+                dynamic np = npnearest.NPNearest(@"C:\Users\conta\CVC\3C-Evolution\git3ce\data\data.h.pickle");
+                dynamic res = np.search(164113, 10);
+                Console.WriteLine(res);
+
+                dynamic npparser = Py.Import("npparser");
+                dynamic p = npparser.NPParser();
+                p.parse(@"C:\Users\conta\CVC\3C-Evolution\git3ce\data\data.txt");
+                p.normalize();
+                p.h();
+                p.save(prefix: "net");
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
