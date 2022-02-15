f864180ce3de9319cce3098dbddf3f75274657c5
Cyril Vincent <contact@cyrilvincent.com>
2022-02-15 19:51:13 +0100

    1.2

--- npcartemplate.py
+++ npcartemplate.py
@@ -12,14 +12,10 @@ from npproductnearest import NPNearest
 
 
 class NPCarTemplate:
-    """
-    High level class, main program
-    """
-
-    lock = threading.RLock()
 
     def __init__(self, np: NPNearest):
         self.np = np
+        self.todo = "{{TODO}}"
 
     def search_longest_starts_with(self, s1: str, s2: str) -> str:
         res = len(s1)
@@ -52,46 +48,50 @@ class NPCarTemplate:
         elif c2.val == "":
             return c1.val
         res = self.search_match(c1.val, c2.val)
-        if res[1] != "":
-            return res[0] + " {{TODO}} " + res[1]
+        if len(res[1]) > 1 :
+            return res[0] + self.todo + res[1]
+        elif res[0] != c1.val:
+            return res[0] + self.todo
         else:
             return res[0]
 
-    def get_templates_from_cars(self, cars: List[int], cars1: Dict[int, Car], cars2: Dict[int, Car]) -> Dict[int, str]:
+    def get_templates_from_cars(self, cars1: Dict[int, Car], cars2: Dict[int, Car]) -> Dict[int, str]:
         res: Dict[int, str]  = {}
-        for cid in cars:
-            if cid in cars1 and cid in cars2:
+        for cid in cars1.keys():
+            if cid in cars2:
                 res[cid] = self.get_template_by_car(cars1[cid], cars2[cid])
-            elif cid in cars2:
-                res[cid] = cars2[cid].val
-            elif cid in cars1:
-                res[cid] = cars1[cid].val
             else:
-                res[cid] = ""
+                res[cid] = cars1[cid].val
         return res
 
     def get_templates(self, fid: int) -> Dict[int, str]:
         pids = [k for k in self.np.db.keys() if self.np.db[k].fid == fid]
-        if len(pids) == 0:
+        if len(pids) < 2:
             return {}
-        if len(pids) == 1:
-            res: Dict[int, str] = {}
-            for c in self.np.db[pids[0]].l:
-                res[c.id] = c.val
-        else:
-            cars1: Dict[int, Car] = {}
-            cars: List[int] = []
-            for c in self.np.db[pids[0]].l:
-                cars1[c.id] = c
-                cars.append(c.id)
-            cars2: Dict[int, Car] = {}
-            for c in self.np.db[pids[1]].l:
-                cars2[c.id] = c
-            return self.get_templates_from_cars(cars, cars1, cars2)
+        cars1: Dict[int, Car] = {}
+        for c in self.np.db[pids[0]].l:
+            cars1[c.id] = c
+        cars2: Dict[int, Car] = {}
+        for c in self.np.db[pids[-1]].l:
+            cars2[c.id] = c
+        res = self.get_templates_from_cars(cars1, cars2)
+        for i in range(1, min(10, len(pids)) - 1):
+            carsi: Dict[int, Car] = {}
+            for c in self.np.db[pids[i]].l:
+                carsi[c.id] = c
+            templates = self.get_templates_from_cars(cars1, carsi)
+            for k in templates.keys():
+                if self.todo in templates[k]:
+                    if self.todo not in res[k] or (len(templates[k]) < len(res[k])):
+                        res[k] = templates[k]
+                elif templates[k] == "":
+                    res[k] = ""
+        return res
+
 
 if __name__ == '__main__':
     npn = NPNearest("data/chuv-light.h.pickle")
     np = NPCarTemplate(npn)
-    res = np.get_templates(1)
+    res = np.get_templates(2)
     print(res)
 
